3d8df3de7f243824cf7e5ecc69e262052044d98e
meagan-davies <128192488+meagan-davies@users.noreply.github.com>
2024-06-09 22:55:24 -0600

    Testing Frozen-flask

--- app.py
+++ app.py
@@ -4,23 +4,36 @@ from pathlib import Path
 from flask import Flask, render_template
 from flask_frozen import Freezer
 
+import sys
+from flask_flatpages import FlatPages
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.html'
 
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
+
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
+
+
 #app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 freezer = Freezer(app)
 
-@app.cli.command()
-def freeze():
-    freezer.freeze()
 
-@app.cli.command()
-def serve():
-    freezer.run()
+# @app.cli.command()
+# def freeze():
+#     freezer.freeze()
+
+# @app.cli.command()
+# def serve():s
+#     freezer.run()
 
 @app.route('/')
 def home():
@@ -32,4 +45,7 @@ def pages(page):
 
 # Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
-    app.run(port=8080)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8080)
